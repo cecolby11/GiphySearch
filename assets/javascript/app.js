7d851e512d5b8d80f27d555fe052cc1dd1e3f29f
cecolby11 <cecolby11@users.noreply.github.com>
2017-03-05 19:41:44 -0600

    ajax query, dynamically generating buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,70 @@
 // APP STATE
 // =========
 
-// array of button topics 
+var appState;
+    
+function resetAppState() {
+  return {
+    'selectedTopic': null, //from selected button for search
+    'topics': ['kitty','puppy', 'obama', 'joey tribbiani']
+  };
+}
+
+function initializeApp() {
+  appState = resetAppState();
+}
+
+// ===============
+// FUNCTIONS/LOGIC
+// ===============
+
+  var selectedTopic = "kitty"; //TODO get from user-input
+
+  var queries = {
+    // Vars for data
+    'searchEndpoint': 'http://api.giphy.com/v1/gifs/search?',
+    'numResults': '&limit=10',
+    'apiKey': '&api_key=dc6zaTOxFJmzC', // put this at end of query
+
+    processGiphyResponse: function(resultsArr) {
+      for(var i = 0; i < resultsArr.length; i++) {
+        var resultObject = {
+          'id': resultsArr[i].id,
+          'staticUrl': resultsArr[i].images.fixed_height_still.url,
+          'animatedUrl': resultsArr[i].images.original.url, 
+          'rating': resultsArr[i].rating 
+        };
+        console.log(resultObject);
+      }
+      //display the static url 
+    },
+
+    //AJAX request to giphy API - return 10 objects
+    getGifs: function(topic) {
+      // parameter q=xyz - search term
+      var searchTerm = '&q=' + topic;
+      var queryURL = this.searchEndpoint +searchTerm + this.numResults + this.apiKey;
+
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      .error(function() {
+        console.log("ERROR: bad API call");
+      })
+      .done(function(response){
+        let resultsArray = response.data; //array of objects (each a gif)
+        queries.processGiphyResponse(resultsArray);
+      });
+    }
+
+  };
+
+  // last step
+  // add user form value to topics array 
+  // call refresh display to refresh the buttons
+
+$(document).ready(function() {
 
 // ================
 // EVENT MANAGEMENT
@@ -17,35 +80,28 @@
 
 // last step
 // get value from user input box in form
-
-// ===============
-// FUNCTIONS/LOGIC
-// ===============
-
-// GET THE GIFs 
-// AJAX request to giphy API 
-  // api key
-// get 10 static non-animated images and display them
-  // q parameter
-  // limit parameter
-// get the rating for display
-  // rating parameter 
-// static and non-static links 
-
-// last step
-// add user form value to topics array 
-// call refresh display to refresh the buttons
-
-
+// trim response 
 
 // ========
 // DISPLAY
 // ========
 
-// dynamically create buttons on page load 
-// FOR LOOP
-// button for each topic in the array
-// set text to the topic from the array
+  var browser = {
+
+    renderButtons: function(arr) {
+      var buttonSection = $('.gif-button-section');
+      buttonSection.empty(); //remake all buttons every time called 
+
+      for(var i = 0; i < arr.length; i++){
+        var newButton = $("<button></button>");
+        newButton.html(arr[i]);
+        newButton.addClass('gif-button');
+        newButton.attr('data-name',arr[i]);
+        buttonSection.append(newButton);
+      }
+    }
+
+  };
 
 // display gifs 
 // should empty the section displaying them first so when user clicks  new button gifs are replaced not appended to previous
@@ -62,11 +118,14 @@
 // INTIALIZE APP
 // =============
 
+  initializeApp();
+  queries.getGifs(selectedTopic);
+  browser.renderButtons(appState.topics);
+
+});
 
 
-
-
-
+//=====================================
 // in order: 
 
 // array of button topics
@@ -92,3 +151,4 @@
 // get value from user input box
 // add it to topics array
 // remake the buttons on the page, including that new topic 
+
