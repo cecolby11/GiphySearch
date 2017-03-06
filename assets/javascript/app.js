3541181540eea3c0b9269a07f29489891f0fd261
cecolby11 <cecolby11@users.noreply.github.com>
2017-03-05 22:00:50 -0600

    user input form

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,8 +23,6 @@ $(document).ready(function() {
 // FUNCTIONS/LOGIC
 // ===============
 
-  var selectedTopic = 'kitty'; //TODO get from user-input
-
   var queries = {
     // Vars for data
     'searchEndpoint': 'http://api.giphy.com/v1/gifs/search?',
@@ -32,7 +30,7 @@ $(document).ready(function() {
     'apiKey': '&api_key=dc6zaTOxFJmzC', // put this at end of query
 
     processGiphyResponse: function(resultsArr) {
-      appState.currentGifArray = []; // reset storage array to empty first
+      appState.currentGifArray = []; //empty gif array first
       for(var i = 0; i < resultsArr.length; i++) {
         var resultObject = {
           'id': resultsArr[i].id,
@@ -41,7 +39,6 @@ $(document).ready(function() {
           'rating': resultsArr[i].rating,
           'state': 'static'
         };
-        //reset gif array
         //append gifs for this topic
         appState.currentGifArray.push(resultObject);
       }
@@ -66,13 +63,19 @@ $(document).ready(function() {
         let resultsArray = response.data; //array of objects (each a gif)
         queries.processGiphyResponse(resultsArray);
       });
+    }, 
+
+    addNewUserTopic: function(userInput){
+      if(($.inArray(userInput, appState.topicsArray)) > 0){
+        alert(userInput + ' was already added, try adding a new topic!');
+      } else { //if not already in topic array
+        appState.topicsArray.push(userInput);
+        appState.phase = 'addNewUserTopic'
+        browser.refreshDisplay();
+      }
     }
 
   };
-
-  // last step
-  // add user form value to topics array 
-  // call refresh display to refresh the buttons
 
 // ================
 // EVENT MANAGEMENT
@@ -81,21 +84,22 @@ $(document).ready(function() {
 // user clicks topic button
 // jquery event management so listener is added to buttons created in future too
 $(document).on('click', '.topic-button', function() {
-    console.log($(this).attr('data-name'));
     appState.selectedTopic = $(this).attr('data-name');
     queries.sendGifRequest(appState.selectedTopic);
     appState.phase = 'topicSelected';
 });
 
-// user clicks a gif
+// user clicks a gif image
 // call function to animate/static-ify the gif 
 $(document).on('click', '.gif-image', function() {
   browser.playPauseGif($(this));
 });
 
-// last step
-// get value from user input box in form
-// trim response 
+$('.submit-topic').on('click', function(){
+  event.preventDefault();
+  var userInput = $('#topic-input').val().trim();
+  queries.addNewUserTopic(userInput);
+});
 
 // ========
 // DISPLAY
@@ -174,17 +178,6 @@ $(document).on('click', '.gif-image', function() {
 
   };
 
-// display gifs 
-// should empty the section displaying them first so when user clicks  new button gifs are replaced not appended to previous
-// first display as static 
-// include a data-state = 'static' and the static/animated links. 
-// change which is which with an if statement? 
-// display gif ratings underneath
-
-// last step
-// add form to page
-// refresh buttons displayed when user clicks submit button 
-
 // =============
 // INTIALIZE APP
 // =============
@@ -195,23 +188,6 @@ $(document).on('click', '.gif-image', function() {
 
 
 //=====================================
-// in order: 
-
-// get 10 static non-animated images and display them
-  // q parameter
-  // limit parameter
-// if user clicks another button, the gifs should get replaced, don't just keep appending more gifs 
-// display gif ratings underneath
-  // rating parameter
-// when user clicks gif image, animate the gif. 
-// if user clicks again, stop playing and be static again. 
-
-// last step
-// add form to page
-// get value from user input box
-// add it to topics array
-// remake the buttons on the page, including that new topic 
-
 
 //PHASES: 
 // phase starts as initialize when initializeApp function called 
